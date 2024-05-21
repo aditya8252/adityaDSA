79d5b9325aa33a05ca73a08f5c9f77f93dfe0263
aditya8252 <aditya.awari@technomile.com>
2024-05-21 23:24:50 +0530

    problem 27= need some changes

---
+++ src/pkg1/Remove_element_27.java
@@ -0,0 +1,31 @@
+package pkg1;
+
+public class Remove_element_27 {
+	
+	static int removeElement(int[] nums, int val)
+	{
+		int index = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+        return index;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		int a[]= {3,2,2,3};
+		int b=3;
+		int res = removeElement(a, b);
+		
+		System.out.print("after removing element: " + res);
+
+	}
+
+}
